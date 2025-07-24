92f210213f0ad5876e19f27e593dbad82161c699
Jussivan <jussivanbm1029@gmail.com>
2025-07-24 16:59:29 -0300

    edit hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,8 +5,6 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel"
 import Autoplay from 'embla-carousel-autoplay'
 import { useRef } from "react"
@@ -21,26 +19,31 @@ export default function Hero() {
     )
 
     return (
-        <div className="flex bg-gradient-to-r from-blue-900 via-blue-900 to-blue-900/50 lg:min-h-[30em] justify-center items-center ">
-                <div className="absolute -z-1 right-0 lg:w-[65em] h-[30em] overflow-hidden">
-                    <Carousel plugins={[plugin.current]} opts={{ loop: true }}>
-                        <CarouselContent>
-                            <CarouselItem>
-                                <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2023/05/shutterstock_2029125182-1-min.jpg" alt="Imagem 1"/>
-                            </CarouselItem>
-                        </CarouselContent>
-                    </Carousel>
-                </div>
-            
+        <div className="flex lg:min-h-[30em] justify-center items-center bg-gradient-to-r from-blue-950 via-blue-950 to-[rgba(0,0,0,0)]">
+            <div className="absolute -z-1 right-0 lg:w-[65em] h-[30em] overflow-hidden">
+                <Carousel plugins={[plugin.current]} opts={{ loop: true }}>
+                    <CarouselContent  className="transition-transform duration-1000 ease-in-out">
+                        <CarouselItem>
+                            <img src="https://assets-blog.pagseguro.uol.com.br/wp-content/2023/05/shutterstock_2029125182-1-min.jpg" alt="Imagem 1"/>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <img src="https://www.meuvademecumonline.com.br/blog/wp-content/uploads/2024/10/exemplos-de-empresas-que-usam-crm.jpeg" alt="Imagem 2"/>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <img src="https://media.licdn.com/dms/image/v2/C4D12AQFKpLurhUjnpg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1649361497775?e=2147483647&v=beta&t=nA_gEZwezvLuZi-m1OWR7QVXmuBZzvv5MDu6TaFH0T8" alt="Imagem 3"/>
+                        </CarouselItem>
+                    </CarouselContent>
+                </Carousel>
+            </div>
             <section className="mx-auto w-full flex flex-row items-center max-w-7xl">
                 <div className="w-1/2 flex flex-col items-start gap-10 px-25">
-                    <h1 className="text-4xl font-bold text-white">
+                    <h1 className="text-4xl font-bold text-(--background)">
                         Soluções que vão além do código, além do óbvio.
                     </h1>
-                    <h4 className="text-lg">
+                    <h4 className="text-lg text-(--background)">
                         Transforme ideias em resultados com tecnologia, criatividade e inovação.
                     </h4>
-                    <Button variant="default" size="lg" className="px-8">
+                    <Button variant="default" size="lg" className="w-full">
                         Fale conosco
                     </Button>
                 </div>
